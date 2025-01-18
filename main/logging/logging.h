8563020dbd7389f8975ac92e730d3eb9d2c649d5
Dakshit Babbar <dakshba@amazon.com>
2025-01-19 00:30:19 +0530

    Add LogDebug and LogError functionality

--- main/logging/logging.h
+++ main/logging/logging.h
@@ -3,9 +3,10 @@
 
 #include "remote_configuration.h"
 
-// #define LogInfo( tag, message, ... ) (ENABLE_LOGGING == 1)? ( ESP_LOGI( tag, message, ##__VA_ARGS__ ) ) : ()
-// #define LogInfo( tag, message, ... ) ESP_LOGI( tag, message, ##__VA_ARGS__ )
-
 #define LogInfo( tag, message, ... ) if( ENABLE_LOGGING == 1 ) ESP_LOGI( tag, message, ##__VA_ARGS__ )
+
+#define LogDebug( tag, message, ... ) if( ENABLE_LOGGING == 1 ) ESP_LOGD( tag, message, ##__VA_ARGS__ )
+
+#define LogError( tag, message, ... ) if( ENABLE_LOGGING == 1 ) ESP_LOGE( tag, message, ##__VA_ARGS__ )
 
 #endif
